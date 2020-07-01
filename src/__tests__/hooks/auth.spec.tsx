97ff3a29091bf51a4c9b778a92f93792663e70c1
Leandro Rocha <prog.leandro@gmail.com>
2020-07-01 09:50:47 -0300

    Iniciando o teste dos hooks

---
+++ src/__tests__/hooks/auth.spec.tsx
@@ -0,0 +1,16 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useAuth, AuthProvider } from '../../hooks/auth';
+
+describe('Auth hook', () => {
+  it('should to be able to sign in', () => {
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+    result.current.signIn({
+      email: 'johndoe@example.com.br',
+      password: '123456',
+    });
+
+    expect(result.current.user.email).toEqual('johndoe@example.com.br');
+  });
+});
